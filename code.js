e3d5c6374781dd17cd5d8115572f3ec43e1cdd92
Joshua Tilling <jetilling@Joshua-Tillings-MacBook-Pro.local>
2016-08-15 14:42:41 -0600

    fourth commit

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 var journal = ["DM13 rocks! Woo!", "the journal", "August", 15, 2016];
+
+/*
+cd foldername
+cd ..
+cd ~
+ls
+mkdir newfileName
+touch newFileName.txt
+
+git clone
+git status
+git add filename.js
+git commit -m "message"
+git push
+
+*/
